2663e6983af94bd9d5a5cb473c3ae2c212733962
ddbruce <daniel@danieldbruce.com>
2016-01-26 00:51:04 -0500

    added scrolling, shortcuts, readme.md

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ZettaPrompt
-A JavaScript-based, keyboard-controlled teleprompter application
+ZettaPrompt is a JavaScript-based, keyboard-controlled teleprompter application. It can be used without a web server, run directly from `index.html`.
+
+Shortcut keys are as follows:
+
+|Key|Command|
+|:-:|-------|
+|`f`|Scroll through script forward|
+|`shift`+`f` or `ctrl`+`f`|Scrolls script forward more quickly|
+|`ctrl`+`shift`+`f`| Scrolls script forward even quicker|
+|`r`|Scroll scrough script backward|
+|`shift`+`r` or `ctrl`+`r`|Scrolls script backward more quickly|
+|`ctrl`+`shift`+`r`| Scrolls script backward even quicker|
+|`space`|Starts/stops scrollng<sup>1</sup>|
+|`down`|Increases forward scroll rate|
+|`shift`+`down`|Scrubs script forward by 150 pixels|
+|`up`|Decreases forward scroll rate<sup>2</sup>|
+|`shift`+`up`|Scrubs script backward by 150 pixels|
+
+
+Notes:  
+<sup>1</sup> `space` will resume scrolling at the same speed previously used. If there was no preset scrolling speed, it will scroll at the slowest possible speed.  
+<sup>2</sup> `up`, if used when the text is stopped, will scroll backwards.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $(function() {
 
+	var speed = 0;
+	var savedspeed = 0;
+
 	$.urlParam = function(name){
 		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
 		if (results == null) return null;
@@ -30,12 +33,72 @@ $(function() {
 
 	$("#script-search-input").focus();
 
-	$('#script-search-input').keypress(function(e) {
+	$('#script-search-input').keydown(function(e) {
 		if (e.which == 13) {
 			e.preventDefault();
 			var searchinput = $("#script-search-input").val();
 			window.location = window.location.href + "?file=" + searchinput;
 		}
 	});
-});
 
+	$(document).keydown(function(e) {
+		if (e.which == 32) { //space
+			e.preventDefault();
+			if (speed == 0 && savedspeed != 0) {
+				speed = savedspeed;
+			}
+			else if (speed != 0) {
+				savedspeed = speed;
+				speed = 0;
+			}
+			else if (speed == 0 && savedspeed == 0) {
+				speed = 1;
+			}
+		}
+		else if (e.which == 40 && !e.shiftKey) { //down
+			e.preventDefault();
+			speed += 1;
+		}
+		else if (e.which == 40 && e.shiftKey) { // shift+down (scrubs down)
+			e.preventDefault();
+			window.scrollBy(0,150);
+		}
+		else if (e.which == 38 && !e.shiftKey) { //up
+			e.preventDefault();
+			speed -= 1;
+		}
+		else if (e.which == 38 && e.shiftKey) { // shift+up (scrubs up)
+			e.preventDefault();
+			window.scrollBy(0,-150);
+		}
+		else if (e.which == 70 && !e.shiftKey && !e.ctrlKey) { // f
+			e.preventDefault();
+			speed = 2;
+		}
+		else if ((e.which == 70 && e.shiftKey && !e.ctrlKey) != (e.which == 70 && !e.shiftKey && e.ctrlKey)) { // shift+f XOR ctrl+f
+			e.preventDefault();
+			speed = 3;
+		}
+		else if (e.which == 70 && e.shiftKey && e.ctrlKey) { // ctrl+shift+f
+			e.preventDefault();
+			speed = 4;
+		}
+		else if (e.which == 82 && !e.shiftKey && !e.ctrlKey) { // r
+			e.preventDefault();
+			speed = -2;
+		}
+		else if ((e.which == 82 && e.shiftKey && !e.ctrlKey) != (e.which == 82 && !e.shiftKey && e.ctrlKey)) { // shift+r XOR ctrl+r
+			e.preventDefault();
+			speed = -3;
+		}
+		else if (e.which == 82 && e.shiftKey && e.ctrlKey) { // ctrl+shift+r
+			e.preventDefault();
+			speed = -4;
+		}
+	});
+
+	setInterval(function() {
+		window.scrollBy(0,1*speed);
+	}, 33);
+
+});
